c3124721675a2e49ac73b9f6900dfcc3c9d89453
Moss91 <47772123+Moss91@users.noreply.github.com>
2019-02-22 09:17:21 +0200

    Add files via upload

---
+++ Goal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice1;
+
+/**
+ *
+ * @author TiehoM
+ */
+public class Goal extends GameEvent{
+    public String toString(){
+        return "Goal scored";
+    }
+}
