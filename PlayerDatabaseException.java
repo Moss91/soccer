534581d49599e0bda08f75b8832d359e6f340299
Moss91 <47772123+Moss91@users.noreply.github.com>
2019-02-27 14:03:30 +0200

    Add files via upload

---
+++ PlayerDatabaseException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+import practice1.*;
+
+/**
+ *
+ * @author TiehoM
+ */
+public class PlayerDatabaseException extends Exception{
+    public PlayerDatabaseException(String message){
+        super(message);
+    }
+}
